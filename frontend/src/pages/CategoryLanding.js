090241fa726efbe224dac515fc2638cdf7a64ad9
Able Roy <royson333@gmail.com>
2021-01-05 23:39:29 +0530

    category fetching codes added using useeffect hook

--- frontend/src/pages/CategoryLanding.js
+++ frontend/src/pages/CategoryLanding.js
@@ -1,64 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import './CategoryLanding.css';
+import "./CategoryLanding.css";
 
 const CATEGORIES = [
   {
     categoryId: 1,
-    name: 'category1',
+    name: "category1",
     imageUrl: "https://source.unsplash.com/random",
   },
   {
     categoryId: 2,
-    name: 'category2',
+    name: "category2",
     imageUrl: "https://source.unsplash.com/random",
   },
   {
     categoryId: 3,
-    name: 'category3',
+    name: "category3",
     imageUrl: "https://source.unsplash.com/random",
   },
   {
     categoryId: 4,
-    name: 'category4',
+    name: "category4",
     imageUrl: "https://source.unsplash.com/random",
   },
   {
     categoryId: 5,
-    name: 'category5',
+    name: "category5",
     imageUrl: "https://source.unsplash.com/random",
   },
   {
     categoryId: 6,
-    name: 'category6',
+    name: "category6",
     imageUrl: "https://source.unsplash.com/random",
   },
   {
     categoryId: 7,
-    name: 'category7',
+    name: "category7",
     imageUrl: "https://source.unsplash.com/random",
   },
   {
     categoryId: 8,
-    name: 'category8',
+    name: "category8",
     imageUrl: "https://source.unsplash.com/random",
   },
   {
     categoryId: 9,
-    name: 'category9',
+    name: "category9",
     imageUrl: "https://source.unsplash.com/random",
   },
 ];
 
 const CategoryLanding = () => {
-  return <div className="category-wrapper">
+  //category state
+  const [categories, setCategory] = useState();
+
+  //api call to fetch categories
+  useEffect(() => {
+    fetch("http://localhost:5000/api/categorys", {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+      .then((res) => console.log(res.json()))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="category-wrapper">
       {CATEGORIES.map((category) => {
-          return <div className="category-container" id={category.categoryId}>
-                <h1>{category.name}</h1>
-                <img src={category.imageUrl} alt={category.name}/>
+        return (
+          <div className="category-container" id={category.categoryId}>
+            <h1>{category.name}</h1>
+            <img src={category.imageUrl} alt={category.name} />
           </div>
+        );
       })}
-  </div>;
+    </div>
+  );
 };
 
 export default CategoryLanding;
