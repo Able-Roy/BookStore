9821552140c3234b35441fcc80faaafd57921d85
Able Roy <royson333@gmail.com>
2021-01-07 00:49:35 +0530

    cors headers were added

--- Backend/app.js
+++ Backend/app.js
@@ -15,12 +15,12 @@ app.use(bodyParser.json());
 
 // set initial headers
 app.use((req, res, next) => {
-  res.setHeader('Acess-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTION');
   next();
 });
 
