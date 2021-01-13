e103db52a7b0e17709d32ccab3b2d3e4a48bf072
Able Roy <royson333@gmail.com>
2021-01-14 00:13:40 +0530

    code added for filter  list looping

--- frontend/src/pages/ProductFilter.js
+++ frontend/src/pages/ProductFilter.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
+const FILTERS = {
+    c1:['s1', 's2', 's3'],
+    c2:['s1', 's2', 's3'],
+    c3:['s1', 's2', 's3'],
+    c4:['s1', 's2', 's3'],
+};
+
 //product list was added
 const ProductFilter () =>{
+    let filterList;
+    for(category in FILTERS){
+        filterList = 
+    }
     return(<div className="filter-wrapper">
+        {
+            
+        }
         </div>);
 }
 
