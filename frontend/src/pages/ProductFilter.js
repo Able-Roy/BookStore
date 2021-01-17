57e47a3ad6ab3c940891e4bbc80b7988efb82847
Able Roy <royson333@gmail.com>
2021-01-17 21:59:34 +0530

    filter comonent starting

--- frontend/src/pages/ProductFilter.js
+++ frontend/src/pages/ProductFilter.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
 
+const PRODUCT_FILTER = {
+  p1: ["filter1", "filter2", "filter3"],
+  p2: ["filter1", "filter2", "filter3"],
+  p3: ["filter1", "filter2", "filter3"],
+  p4: ["filter1", "filter2", "filter3"],
+  p5: ["filter1", "filter2", "filter3"],
+};
+
+const getFilters = (filters) => {
+    let content = [];
+  for (let filter in filters) {
+      console.log(filter);
+    for (let item in filters[filter]) {
+        console.log(filters[filter][item]);
+        
+        content.push(
+          <React.Fragment>
+            <div className="header">{}</div>
+          </React.Fragment>
+        );
+    }
+  }
+};
 //product list was added
-const ProductFilter () =>{
-    return(<div className="filter-wrapper">
-        </div>);
-}
+const ProductFilter = () => {
+  return <div className="filter-wrapper">{getFilters(PRODUCT_FILTER)}</div>;
+};
 
 export default ProductFilter;
