dab43bba28a2c6a6cb025068ac0f81068de27294
Able Roy <royson333@gmail.com>
2021-01-20 23:54:18 +0530

    filter html changes

--- frontend/src/pages/components/ProductFilter.js
+++ frontend/src/pages/components/ProductFilter.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { uuid } from "uuidv4";
 
+import './ProductFilter.css';
 const PRODUCT_FILTER = {
   p1: ["filter1", "filter2", "filter3"],
   p2: ["filter1", "filter2", "filter3"],
@@ -8,25 +10,35 @@ const PRODUCT_FILTER = {
   p5: ["filter1", "filter2", "filter3"],
 };
 
+/*
+  function to create html of filter section
+*/
 const getFilters = (filters) => {
-    let content = [];
+  let content = [];
   for (let filter in filters) {
-    content.push(
-      <div className="header">{filter}</div>
-  );
+    content.push(<div className="header">{filter}</div>);
     for (let item in filters[filter]) {
-        console.log(filters[filter][item]);
-        content.push(
-          <input type="checkbox" value={filters[filter][item]}/>
+      console.log(filters[filter][item]);
+      content.push(
+        <React.Fragment>
+          <input type="checkbox" className="filer-checkbox" value={filters[filter][item]} key={uuid()} />
+          <div className="filter-name" key={uuid()}>
+            {filters[filter][item]}
+          </div>
+        </React.Fragment>
       );
     }
   }
-
   return content;
 };
 //product list was added
 const ProductFilter = () => {
-  return <div className="filter-wrapper">{getFilters(PRODUCT_FILTER)}</div>;
+  return (
+    <div className="filter-wrapper">
+      <div className="filter-title">Filters</div>
+      {getFilters(PRODUCT_FILTER)}
+    </div>
+  );
 };
 
 export default ProductFilter;
